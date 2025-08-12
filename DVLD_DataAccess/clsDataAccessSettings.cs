42b38f1929903b1008967602f7e900b254e57e7f
Sherif Ali <elemlak202099@gmail.com>
2025-08-13 01:21:18 +0300

    first commit

---
+++ DVLD_DataAccess/clsDataAccessSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DVLD_DataAccess
+{
+    static class clsDataAccessSettings
+    {
+
+        //public static string ConnectionString = "Server=.;Database=DVLD;User Id=sa;Password=123456;";
+        public static string ConnectionString =
+    "Server=LAPTOP-0TBD5B1V;Database=DVLD;Integrated Security=True;Connect Timeout=60;Encrypt=True;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False;";
+
+
+    }
+}
